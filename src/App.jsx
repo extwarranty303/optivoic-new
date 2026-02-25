2d30f6fe4df738b89601b8783fe4d6af41ec1240
extwarranty <55749563+extwarranty303@users.noreply.github.com>
2026-02-25 16:02:18 -0500

    app changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Storefront from "./components/Storefront";
 import TemplateDetails from "./components/TemplateDetails";
 import ExecutiveTaxEngine from "./components/ExecutiveTaxEngine";
