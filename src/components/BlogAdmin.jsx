db008edaa3da61ac537421ade311ce8c6005df56
Derek Crosby <55749563+extwarranty303@users.noreply.github.com>
2026-07-06 00:43:36 -0400

    syntax error build issue fix

--- src/components/BlogAdmin.jsx
+++ src/components/BlogAdmin.jsx
@@ -1,4 +1,4 @@
-autheeimport React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { supabase } from '../supabaseClient';
 import Footer from './Footer';
